3d8b6526d8cb3a10cb83918b1ab9dd97c91cb15a
Mikhail.Zaykin <mikkrasnodar@yandex.ru>
2024-01-20 19:47:45 +0300

    http://localhost:8000/api/groups HTTP 200 OK Allow: GET, POST, PATCH, DELETE, HEAD, OPTIONS Content-Type: application/json Vary: Accept
    {
        "groups": [
            {
                "id": 1,
                "name": "1A",
                "curator": {
                    "id": 1,
                    "name": "Михаил",
                    "surname": "Зайкин",
                    "patronymic": "Александрович",
                    "birth_day": "1978-04-25"
                }
            },
            {
                "id": 2,
                "name": "2B",
                "curator": {
                    "id": 1,
                    "name": "Михаил",
                    "surname": "Зайкин",
                    "patronymic": "Александрович",
                    "birth_day": "1978-04-25"
                }
            }
        ]
    }

--- django_learn/mz_app/serializers.py
+++ django_learn/mz_app/serializers.py
@@ -1,16 +1,18 @@
 from mz_app.models import Person, Group
 
-from dynamic_rest.serializers import DynamicModelSerializer
+from dynamic_rest.serializers import DynamicModelSerializer, DynamicRelationField
 
 class PersonSerializer(DynamicModelSerializer):
     class Meta:
         model = Person
-        name = 'person'
+        #name = 'person'
         fields = ('id', 'name', 'surname', 'patronymic', 'birth_day')
 
 
 class GroupSerializer(DynamicModelSerializer):
+    curator = DynamicRelationField(PersonSerializer, embed=True)
+
     class Meta:
         model = Group
-        name = 'group'
-        fields = ('id', 'name', 'curator') # а вот как включить информацию о кураторе???
+        #name = 'group'
+        fields = ('id', 'name', 'curator') 
