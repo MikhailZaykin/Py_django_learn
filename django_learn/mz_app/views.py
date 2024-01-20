9c5ab80d6b3fceb18d922d17696668f6a1352860
Mikhail.Zaykin <mikkrasnodar@yandex.ru>
2024-01-20 17:56:55 +0300

    change /mz_app page text

--- django_learn/mz_app/views.py
+++ django_learn/mz_app/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def another_page(request):
+    return HttpResponse("Some page")
