cdf647cdf5830fe75a3d6b89eaa91ac26a99eea7
Mikhail.Zaykin <mikkrasnodar@yandex.ru>
2024-01-20 15:55:36 +0300

    add mz_add to django_learn/settings.py

--- django_learn/django_learn/settings.py
+++ django_learn/django_learn/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mz_app.apps.NewAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
